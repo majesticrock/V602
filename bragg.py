29691031c1faa9eb53f37519a3fa9ead187525f9
rope1505 <rpelkner@googlemail.com>
2019-04-10 22:38:24 +0200

    eh

--- bragg.py
+++ bragg.py
@@ -10,11 +10,19 @@ c = 299792458
 d = 2.014e-10
 #theta = 4.7 *np.pi/180 
 #theta = 22.5 *np.pi/180
-theta = 20.29 *np.pi/180
+#theta = 20.29 *np.pi/180
+thetab = 20.00 *np.pi/180
+thetaa = 22.21 *np.pi/180
+R = 13.8
+Ea = h * c / (2 * d * np.sin(thetaa))
+Eb = h * c / (2 * d * np.sin(thetab))
+print(Ea, Eb)
+#Wellenlänge
+#l = h *c / E
+#print(l)
+#Abschirmkonstante Kupfer
+sig1 = 29 - np.sqrt(Eb/R)
+print("sig1", sig1)
 
-E = h * c / (2 * d * np.sin(theta))
-print(E)
-E = 0.2
-l = h *c / E
-E = 0.83
-print(l)
+sig2 = 29 - 2 * np.sqrt((R* (29-sig1)**2 - Ea)/R)
+print("sig2", sig2)
